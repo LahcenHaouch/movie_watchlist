f8adddab2bfe162001fb72ac856a21c30dc0cbdd
LahcenHaouch <lahcen.haouch@protonmail.com>
2023-12-16 13:56:55 +0100

    Display list of movies after search

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const OMDB_API = "http://www.omdbapi.com/?apikey=3c882be6";
+const OMDB_API = "http://www.omdbapi.com/?apikey=3c882be6&plot=full";
 
 const formEl = document.querySelector("form");
+const movieListEl = document.querySelector("#movie-list");
 
 formEl.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -13,5 +14,31 @@ formEl.addEventListener("submit", (event) => {
 
   fetch(`${OMDB_API}&${searchParams.toString()}`)
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+      movieListEl.innerHTML = "";
+
+      const { Search } = data;
+
+      Search.forEach((element) => {
+        const { Poster, Title, Year } = element;
+
+        const li = document.createElement("li");
+
+        const img = document.createElement("img");
+        img.setAttribute("src", Poster);
+
+        const div = document.createElement("div");
+
+        const h2 = document.createElement("h2");
+        h2.textContent = Title;
+
+        const p = document.createElement("p");
+        p.textContent = Year;
+
+        div.append(h2, p);
+
+        li.append(img, div);
+        movieListEl.append(li);
+      });
+    });
 });
